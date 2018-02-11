a1333799b56f5c71bed52d4fbab03caa3c5f8e3c
Ryo NIHEI <nihei.dev@gmail.com>
2018-02-11 16:21:35 +0900

    tokenizerのUT追加

--- test/tokenizer_test.c
+++ test/tokenizer_test.c
@@ -91,5 +91,329 @@ void test_1(connie_Connie *c)
         A_EQL_ULONG(c, 13, tkn->pos.col);
     }
 
+    // |
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_PRULE_OR, tkn->type);
+        A_EQL_ULONG(c, 2, tkn->pos.row);
+        A_EQL_ULONG(c, 4, tkn->pos.col);
+    }
+
+    // T
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_NAME, tkn->type);
+        A_EQL_ULONG(c, 2, tkn->pos.row);
+        A_EQL_ULONG(c, 6, tkn->pos.col);
+    }
+
+    // \n
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_NEW_LINE, tkn->type);
+        A_EQL_ULONG(c, 2, tkn->pos.row);
+        A_EQL_ULONG(c, 7, tkn->pos.col);
+    }
+
+    // ;
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_PRULE_TERMINATOR, tkn->type);
+        A_EQL_ULONG(c, 3, tkn->pos.row);
+        A_EQL_ULONG(c, 4, tkn->pos.col);
+    }
+
+    // \n
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_NEW_LINE, tkn->type);
+        A_EQL_ULONG(c, 3, tkn->pos.row);
+        A_EQL_ULONG(c, 5, tkn->pos.col);
+    }
+
+    // T
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_NAME, tkn->type);
+        A_EQL_ULONG(c, 4, tkn->pos.row);
+        A_EQL_ULONG(c, 0, tkn->pos.col);
+    }
+
+    // \n
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_NEW_LINE, tkn->type);
+        A_EQL_ULONG(c, 4, tkn->pos.row);
+        A_EQL_ULONG(c, 1, tkn->pos.col);
+    }
+
+    // :
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_PRULE_LEADER, tkn->type);
+        A_EQL_ULONG(c, 5, tkn->pos.row);
+        A_EQL_ULONG(c, 4, tkn->pos.col);
+    }
+
+    // T
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_NAME, tkn->type);
+        A_EQL_ULONG(c, 5, tkn->pos.row);
+        A_EQL_ULONG(c, 6, tkn->pos.col);
+    }
+
+    // '*'
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_STRING, tkn->type);
+        A_EQL_ULONG(c, 5, tkn->pos.row);
+        A_EQL_ULONG(c, 8, tkn->pos.col);
+    }
+
+    // F
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_NAME, tkn->type);
+        A_EQL_ULONG(c, 5, tkn->pos.row);
+        A_EQL_ULONG(c, 12, tkn->pos.col);
+    }
+
+    // \n
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_NEW_LINE, tkn->type);
+        A_EQL_ULONG(c, 5, tkn->pos.row);
+        A_EQL_ULONG(c, 13, tkn->pos.col);
+    }
+
+    // |
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_PRULE_OR, tkn->type);
+        A_EQL_ULONG(c, 6, tkn->pos.row);
+        A_EQL_ULONG(c, 4, tkn->pos.col);
+    }
+
+    // F
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_NAME, tkn->type);
+        A_EQL_ULONG(c, 6, tkn->pos.row);
+        A_EQL_ULONG(c, 6, tkn->pos.col);
+    }
+
+    // \n
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_NEW_LINE, tkn->type);
+        A_EQL_ULONG(c, 6, tkn->pos.row);
+        A_EQL_ULONG(c, 7, tkn->pos.col);
+    }
+
+    // ;
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_PRULE_TERMINATOR, tkn->type);
+        A_EQL_ULONG(c, 7, tkn->pos.row);
+        A_EQL_ULONG(c, 4, tkn->pos.col);
+    }
+
+    // \n
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_NEW_LINE, tkn->type);
+        A_EQL_ULONG(c, 7, tkn->pos.row);
+        A_EQL_ULONG(c, 5, tkn->pos.col);
+    }
+
+    // F
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_NAME, tkn->type);
+        A_EQL_ULONG(c, 8, tkn->pos.row);
+        A_EQL_ULONG(c, 0, tkn->pos.col);
+    }
+
+    // \n
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_NEW_LINE, tkn->type);
+        A_EQL_ULONG(c, 8, tkn->pos.row);
+        A_EQL_ULONG(c, 1, tkn->pos.col);
+    }
+
+    // :
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_PRULE_LEADER, tkn->type);
+        A_EQL_ULONG(c, 9, tkn->pos.row);
+        A_EQL_ULONG(c, 4, tkn->pos.col);
+    }
+
+    // '('
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_STRING, tkn->type);
+        A_EQL_ULONG(c, 9, tkn->pos.row);
+        A_EQL_ULONG(c, 6, tkn->pos.col);
+    }
+
+    // E
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_NAME, tkn->type);
+        A_EQL_ULONG(c, 9, tkn->pos.row);
+        A_EQL_ULONG(c, 10, tkn->pos.col);
+    }
+
+    // ')'
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_STRING, tkn->type);
+        A_EQL_ULONG(c, 9, tkn->pos.row);
+        A_EQL_ULONG(c, 12, tkn->pos.col);
+    }
+
+    // \n
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_NEW_LINE, tkn->type);
+        A_EQL_ULONG(c, 9, tkn->pos.row);
+        A_EQL_ULONG(c, 15, tkn->pos.col);
+    }
+
+    // |
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_PRULE_OR, tkn->type);
+        A_EQL_ULONG(c, 10, tkn->pos.row);
+        A_EQL_ULONG(c, 4, tkn->pos.col);
+    }
+
+    // id
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_NAME, tkn->type);
+        A_EQL_ULONG(c, 10, tkn->pos.row);
+        A_EQL_ULONG(c, 6, tkn->pos.col);
+    }
+
+    // \n
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_NEW_LINE, tkn->type);
+        A_EQL_ULONG(c, 10, tkn->pos.row);
+        A_EQL_ULONG(c, 8, tkn->pos.col);
+    }
+
+    // ;
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_PRULE_TERMINATOR, tkn->type);
+        A_EQL_ULONG(c, 11, tkn->pos.row);
+        A_EQL_ULONG(c, 4, tkn->pos.col);
+    }
+
+    // \n
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_NEW_LINE, tkn->type);
+        A_EQL_ULONG(c, 11, tkn->pos.row);
+        A_EQL_ULONG(c, 5, tkn->pos.col);
+    }
+
+    // id
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_NAME, tkn->type);
+        A_EQL_ULONG(c, 13, tkn->pos.row);
+        A_EQL_ULONG(c, 0, tkn->pos.col);
+    }
+
+    // \n
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_NEW_LINE, tkn->type);
+        A_EQL_ULONG(c, 13, tkn->pos.row);
+        A_EQL_ULONG(c, 2, tkn->pos.col);
+    }
+
+    // :
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_PRULE_LEADER, tkn->type);
+        A_EQL_ULONG(c, 14, tkn->pos.row);
+        A_EQL_ULONG(c, 4, tkn->pos.col);
+    }
+
+    // '[a-zA-Z_][a-zA-Z0-9_]*'
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_STRING, tkn->type);
+        A_EQL_ULONG(c, 14, tkn->pos.row);
+        A_EQL_ULONG(c, 6, tkn->pos.col);
+    }
+
+    // \n
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_NEW_LINE, tkn->type);
+        A_EQL_ULONG(c, 14, tkn->pos.row);
+        A_EQL_ULONG(c, 30, tkn->pos.col);
+    }
+
+    // ;
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_PRULE_TERMINATOR, tkn->type);
+        A_EQL_ULONG(c, 15, tkn->pos.row);
+        A_EQL_ULONG(c, 4, tkn->pos.col);
+    }
+
+    // \n
+    tkn = good_consume_token(tknzr);
+    A_NOT_NULL(c, tkn);
+    if (tkn != NULL) {
+        A_EQL_INT(c, good_TKN_NEW_LINE, tkn->type);
+        A_EQL_ULONG(c, 15, tkn->pos.row);
+        A_EQL_ULONG(c, 5, tkn->pos.col);
+    }
+
     good_delete_tokenizer(tknzr);
 }

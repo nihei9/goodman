3f20cd8ecb6a4002783cdceb5806926fd7d5ec4e
Ryo NIHEI <nihei.dev@gmail.com>
2018-03-04 00:36:21 +0900

    AST構造変更に合わせて修正

--- src/ast_converter.c
+++ src/ast_converter.c
@@ -7,6 +7,8 @@
 
 #define MAX_RHS_LEN 1024
 
+static int good_is_terminal_symbol_def(const good_AST *prule_ast);
+
 const grm_Grammar *good_convert_ast_to_grammar(const good_AST *root_ast, const grm_SymbolTable *symtbl)
 {
     grm_Grammar *grm = NULL;
@@ -32,11 +34,15 @@ const grm_Grammar *good_convert_ast_to_grammar(const good_AST *root_ast, const g
             continue;
         }
 
+        if (good_is_terminal_symbol_def(prule_ast) != 0) {
+            continue;
+        }
+
         lhs_ast = good_get_child(prule_ast, LHS_OFFSET);
         if (lhs_ast == NULL) {
             goto FAILURE;
         }
-        lhs_str = grm_lookup_in_symtbl(symtbl, lhs_ast->value.token.value.symbol_id);
+        lhs_str = grm_lookup_in_symtbl(symtbl, lhs_ast->token.value.symbol_id);
         if (lhs_str == NULL) {
             goto FAILURE;
         }
@@ -64,7 +70,7 @@ const grm_Grammar *good_convert_ast_to_grammar(const good_AST *root_ast, const g
         if (lhs_ast == NULL) {
             goto FAILURE;
         }
-        lhs_str = grm_lookup_in_symtbl(symtbl, lhs_ast->value.token.value.symbol_id);
+        lhs_str = grm_lookup_in_symtbl(symtbl, lhs_ast->token.value.symbol_id);
         if (lhs_str == NULL) {
             goto FAILURE;
         }
@@ -81,7 +87,7 @@ const grm_Grammar *good_convert_ast_to_grammar(const good_AST *root_ast, const g
                     goto FAILURE;
                 }
 
-                rhs_elem_str = grm_lookup_in_symtbl(symtbl, rhs_elem_ast->value.token.value.symbol_id);
+                rhs_elem_str = grm_lookup_in_symtbl(symtbl, rhs_elem_ast->token.value.symbol_id);
                 if (rhs_elem_str == NULL) {
                     goto FAILURE;
                 }
@@ -104,4 +110,26 @@ FAILURE:
     grm_delete(grm);
 
     return NULL;
+}
+
+static int good_is_terminal_symbol_def(const good_AST *prule_ast)
+{
+    const good_AST *rhs_ast;
+
+    // 全ての生成規則の右辺値が1つの要素のみで構成されており、かつその要素が文字列の場合は、
+    // その生成規則の左辺値の記号は終端記号と判定する。
+    for (rhs_ast = good_get_child(prule_ast, RHS_OFFSET); rhs_ast != NULL; rhs_ast = rhs_ast->brother) {
+        const good_AST *elem;
+
+        if (good_count_child(rhs_ast) > 1) {
+            return 0;
+        }
+
+        elem = good_get_child(rhs_ast, 0);
+        if (elem->type != good_AST_PRULE_RHS_ELEM_STRING) {
+            return 0;
+        }
+    }
+
+    return 1;
 }

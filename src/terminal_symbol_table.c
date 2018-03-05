b58d0ab2aa10df9c212ba7550dd2e2823f24df19
Ryo NIHEI <nihei.dev@gmail.com>
2018-03-06 02:12:26 +0900

    終端記号管理テーブルを追加

---
+++ src/terminal_symbol_table.c
@@ -0,0 +1,72 @@
+#include "terminal_symbol_table.h"
+#include <collections.h>
+#include <string.h>
+
+struct good_TerminalSymbolTable {
+    hmap_HashMap *formats;
+};
+
+good_TerminalSymbolTable *good_new_tsymtbl(void)
+{
+    good_TerminalSymbolTable *tsymtbl;
+    hmap_HashMap *formats;
+
+    tsymtbl = (good_TerminalSymbolTable *) malloc(sizeof (good_TerminalSymbolTable));
+    if (tsymtbl == NULL) {
+        return NULL;
+    }
+
+    formats = hmap_new(c_TYPE_ULONG, c_TYPE_STRING);
+    if (formats == NULL) {
+        free(tsymtbl);
+
+        return NULL;
+    }
+
+    tsymtbl->formats = formats;
+
+    return tsymtbl;
+}
+
+void good_delete_tsymtbl(good_TerminalSymbolTable *tsymtbl)
+{
+    if (tsymtbl == NULL) {
+        return;
+    }
+
+    // TODO good_put_tsym()内でformatに割り当てたメモリ領域を解放する。
+
+    hmap_delete(tsymtbl->formats);
+    tsymtbl->formats = NULL;
+    free(tsymtbl);
+}
+
+int good_put_tsym(good_TerminalSymbolTable *tsymtbl, grm_SymbolID tsym_id, const char *format)
+{
+    const char *dup_format;
+    void *ret;
+
+    dup_format = strdup(format);
+    if (dup_format == NULL) {
+        return 1;
+    }
+
+    ret = hmap_put(tsymtbl->formats, &tsym_id, &dup_format);
+    if (ret == NULL) {
+        return 1;
+    }
+
+    return 0;
+}
+
+const char *good_get_tsym_format(const good_TerminalSymbolTable *tsymtbl, grm_SymbolID tsym_id)
+{
+    void *v;
+
+    v = hmap_lookup(tsymtbl->formats, &tsym_id);
+    if (v == NULL) {
+        return NULL;
+    }
+
+    return *((const char **) v);
+}
